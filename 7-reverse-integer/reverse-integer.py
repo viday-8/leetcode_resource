facc9e168e5f9de9d19be371fc4f406e9cf17ba6
viday-8 <82586251+viday-8@users.noreply.github.com>
2025-05-04 16:41:28 +0530

    Time: 37 ms (69.88%) | Memory: 17.8 MB (36.73%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        num=str(x)
+        length=len(num)
+        start=0
+        new=""
+        new_num=0
+        if num[0]=="-":
+            new+=num[0]
+            start=1
+        print(x>=2**31)
+        for i in range(length-1,start-1,-1):
+            new+=str(int(num[i])%10)
+        y=int(new)
+        if y<-2**31 or y>=2**31:
+            return 0
+        
+        return y
+
+            
+          
