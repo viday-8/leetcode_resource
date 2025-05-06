6d6817f6c87552d2f545c9da69296c349fa87bb8
viday-8 <82586251+viday-8@users.noreply.github.com>
2025-05-06 23:05:30 +0530

    Time: 3 ms (8.15%) | Memory: 18.7 MB (45.01%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,10 +1,11 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         pivot = len(nums)//2
+        moveForward = target > nums[pivot]
         while pivot >= 0 and pivot <= len(nums)-1:
             if nums[pivot] == target:
                return pivot
-            elif target > nums[len(nums)//2]:
+            elif moveForward:
                 pivot +=1
             else :
                 pivot -=1
