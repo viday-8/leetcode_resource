ad31e11db6c5450a325ab23c8dadc20545e955e4
viday-8 <82586251+viday-8@users.noreply.github.com>
2025-05-07 23:34:04 +0530

    Time: 3 ms (3.48%) | Memory: 18.3 MB (73.17%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -2,7 +2,6 @@ class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         pivot = len(nums)//2
         moveForward = target > nums[pivot]
-        diff = 0
         while pivot >= 0 and  pivot <= len(nums)+2//2:
             if nums[pivot] == target:
                 return pivot
@@ -10,19 +9,11 @@ class Solution:
                 return len(nums)
             elif target < nums[0]:
                 return 0        
-            # elif nums[pivot-1] < target < nums[pivot+1]  :
-            #     print("pivot", pivot)
-            #     return pivot
             elif moveForward:
                 pivot += 1
-                print(nums[pivot-1],nums[pivot])
                 if nums[pivot-1]<target < nums[pivot]:
-                    print("m", pivot)
                     return pivot
-                print("mm",pivot)  
             else:
                 pivot -=1
                 if nums[pivot]<target < nums[pivot+1]:
-                    print("b", pivot+1)
                     return pivot+1 
-                print("BB",pivot) 
