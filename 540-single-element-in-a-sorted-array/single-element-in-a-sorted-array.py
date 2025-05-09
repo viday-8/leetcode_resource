1c99d198dd09f17a2c74869739cb4fd85c4e2c8c
viday-8 <82586251+viday-8@users.noreply.github.com>
2025-05-09 21:11:34 +0530

    Time: 3 ms (16.70%) | Memory: 24.9 MB (17.98%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -8,13 +8,13 @@ class Solution:
             print(start, end)
             if nums[start] == nums[start+1]:
                 start += 2
-            elif nums[start] != nums[start+1]:
+            else:
                 return nums[start]     
                 
             
             if nums[end] == nums[end-1]:
                 end -= 2  
-            elif nums[end] != nums[end-1]:
+            else:
                 return nums[end]    
                   
         return nums[0]          
